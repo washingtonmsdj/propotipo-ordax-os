9501a1ae2cbd1fa3ef2409af19ac2bed5346727b
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-19 10:58:55 -0300

    Move component composition catalog to app layer

---
+++ system/apps/component-catalog.mjs
@@ -0,0 +1,22 @@
+import { validateComponentManifests } from "../contracts/component-manifest.mjs";
+import { appComponentManifests } from "../services/components/manifests/apps.mjs";
+import { coreComponentManifests } from "../services/components/manifests/core.mjs";
+import { internetComponent } from "./internet/component.mjs";
+
+const COMPONENTS = validateComponentManifests([
+  ...coreComponentManifests,
+  ...appComponentManifests,
+  internetComponent,
+]);
+
+const COMPONENT_BY_ID = new Map(
+  COMPONENTS.map((component) => [component.id, component]),
+);
+
+export function listSystemComponents() {
+  return COMPONENTS;
+}
+
+export function getSystemComponent(componentId) {
+  return COMPONENT_BY_ID.get(componentId) ?? null;
+}
